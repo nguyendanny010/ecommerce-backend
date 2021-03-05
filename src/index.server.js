c46443c417703724215760b5d39475c440563fc6
dannny <nguyendanny0@gmail.com>
2021-03-04 22:05:41 -0800

    Added user module

--- src/index.server.js
+++ src/index.server.js
@@ -4,6 +4,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+
+//routes
+const userRoutes = require('./routes/user');
 //environment variable or contstants
 env.config()
 
@@ -11,26 +14,15 @@ mongoose.connect(
     `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.sbbga.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        userCreateIndex: true
     }
 ).then(() => {
     console.log('Database connected');
 })
 app.use(express.json());
+app.use('/api', userRoutes);
 
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Hello from Server'
-    })
-
-})
-
-app.post('/data', (req, res, next) => {
-    res.status(200).json({
-        message: req.body
-    })
-
-})
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
 });

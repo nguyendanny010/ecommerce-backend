658c20b0d4bc8da5d9470165604f14b6f4f925b8
dannny <nguyendanny0@gmail.com>
2021-03-11 00:38:03 -0800

    Started validation checks

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const {signup, signin, requireSignin} = require('../controller/auth');
+const { validateRequest, isRequestValidated } = require('../validators/auth');
 const router = express.Router();
 
-router.post('/signup', signup);
+router.post('/signup', validateRequest, isRequestValidated, signup);
 router.post('/signin', signin);
 /*
 router.post('/profile', requireSignin, (req, res) => {

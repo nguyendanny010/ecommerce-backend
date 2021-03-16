0bb2b1ba511567a82065d47b6dbceb1e1c5c0d33
dannny <nguyendanny0@gmail.com>
2021-03-15 18:00:17 -0700

    Fixed user middleware hanging response

--- src/common-middleware/index.js
+++ src/common-middleware/index.js
@@ -14,7 +14,7 @@ exports.requireSignin = (req, res, next) => {
 
 exports.userMiddleware = (req, res, next) => {
   if(req.user.role !== 'user') return res.status(400).json({ message: "User access denied" });
-
+  next();
 }
 exports.adminMiddleware = (req, res, next) => {
   if(req.user.role !== 'admin') return res.status(400).json({ message: "Admin access denied" });

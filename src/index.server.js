0084acbc888abb2a30206a78f2af1b1a8369cadf
dannny <nguyendanny0@gmail.com>
2021-03-15 18:01:27 -0700

    add product to cart functionality added

--- src/index.server.js
+++ src/index.server.js
@@ -9,6 +9,7 @@ const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin/auth');
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
+const cartRoutes = require('./routes/cart');
 //environment variable or contstants
 env.config()
 
@@ -27,6 +28,7 @@ app.use('/api', authRoutes);
 app.use('/api', adminRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
+app.use('/api', cartRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
